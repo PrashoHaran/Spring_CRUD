2518391c8676dd7a7bf5ef04751f7245c07f1c7b
PrashoHaran <prashoharan8962@gmail.com>
2025-09-27 19:27:25 +0530

    User Details Updated

--- src/main/java/com/example/spring_boot_demo/controllers/UserController.java
+++ src/main/java/com/example/spring_boot_demo/controllers/UserController.java
@@ -1,24 +1,45 @@
 package com.example.spring_boot_demo.controllers;
 
+import com.example.spring_boot_demo.entity.UserEntity;
 import com.example.spring_boot_demo.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.spring_boot_demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
 
     //@GetMapping
     //public String getUsers(){
      //   return "Hello API";
     //}
 
-    @GetMapping
-    public List<User> getUsers(){
-        return Arrays.asList(new User(1L,"John", "john@gmail.com"),new User(2L,"Joe", "joe@gmail.com"),new User(3L,"Alex", "Alex@gmail.com"));
+    //Get User List API
+    @GetMapping //
+    public List<UserEntity> getUsers(){
+
+        //return Arrays.asList(new User(1L,"John", "john@gmail.com"),new User(2L,"Joe", "joe@gmail.com"),new User(3L,"Alex", "Alex@gmail.com"));
+
+        return userRepository.findAll();
+    }
+
+    //Send User Details
+    @PostMapping
+    public UserEntity createUser(@RequestBody UserEntity user) {
+        //System.out.println("User Data:"+user.getName()+","+user.getEmail());
+        return userRepository.save(user);
+    }
+
+    //Get Single User API
+    @GetMapping("/{id}")
+    public Optional<UserEntity> getUserById(@PathVariable Long id){
+        return userRepository.findById(id);
     }
 }
