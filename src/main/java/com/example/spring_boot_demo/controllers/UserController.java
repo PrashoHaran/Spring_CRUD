edf773fff5b70060bad4c973b5c5bf17dde94fed
PrashoHaran <prashoharan8962@gmail.com>
2025-09-27 16:17:47 +0530

    User settings updated

--- src/main/java/com/example/spring_boot_demo/controllers/UserController.java
+++ src/main/java/com/example/spring_boot_demo/controllers/UserController.java
@@ -1,15 +1,24 @@
 package com.example.spring_boot_demo.controllers;
 
+import com.example.spring_boot_demo.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+    //@GetMapping
+    //public String getUsers(){
+     //   return "Hello API";
+    //}
+
     @GetMapping
-    public String getUsers(){
-        return "Hello API";
+    public List<User> getUsers(){
+        return Arrays.asList(new User(1L,"John", "john@gmail.com"),new User(2L,"Joe", "joe@gmail.com"),new User(3L,"Alex", "Alex@gmail.com"));
     }
 }
